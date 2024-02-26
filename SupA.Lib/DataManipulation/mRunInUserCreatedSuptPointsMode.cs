478d21988b2c5f7e7b9e0b4501a037eecdd18be3
VahidDev <vrashidli1@gmail.com>
2024-02-26 12:41:13 +0400

    one more fix

--- SupA.Lib/DataManipulation/mRunInUserCreatedSuptPointsMode.cs
+++ SupA.Lib/DataManipulation/mRunInUserCreatedSuptPointsMode.cs
@@ -39,7 +39,7 @@ namespace SupA.Lib.DataManipulation
             }
 
             // Activity Log Tracking
-            mWriteActivityLog.WriteActivityLog("Import and Prepare 3D Support Data for SupA to Run On", DateTime.Now());
+            mWriteActivityLog.WriteActivityLog("Import and Prepare 3D Support Data for SupA to Run On", DateTime.Now);
 
             // If using P3D output then convert this to the standard import format first
             if (mSubInitializationSupA.pubThreeDModelSoftware == "P3D")
